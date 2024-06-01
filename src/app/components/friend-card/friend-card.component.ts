72529fa59335a4f3dbe4c863f677c5b9887fe1bd
Emmanuel <manuboisset@gmail.com>
2024-06-01 17:31:18 +0200

    adding listing friends friend page

--- src/app/components/friend-card/friend-card.component.ts
+++ src/app/components/friend-card/friend-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -7,6 +7,8 @@ import { NgIf } from '@angular/common';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { SharedModule } from 'primeng/api';
 import { User } from '../../models/user';
+import { FriendsService } from '../../services/friends/friends.service';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
     selector: 'app-friend-card',
@@ -19,15 +21,39 @@ import { User } from '../../models/user';
         NgIf,
         ProgressBarModule,
         SharedModule,
+        DialogModule,
     ],
     templateUrl: './friend-card.component.html',
     styleUrl: './friend-card.component.css',
 })
 export class FriendCardComponent {
     @Input() user: User | undefined;
+    @Output() reload: EventEmitter<any> = new EventEmitter();
+
+    constructor(private friendsService: FriendsService) {}
 
     isLoading: boolean = false;
 
     message: string = '';
     error: string = '';
+
+    visible: boolean = false;
+
+    deleteFriend(friendId: string | undefined) {
+        this.isLoading = true;
+        if (!friendId) {
+            this.isLoading = false;
+            return;
+        }
+        this.friendsService.deleteFriend(friendId).subscribe({
+            next: (_) => {
+                this.isLoading = false;
+                this.reload.emit();
+            },
+            error: (err) => {
+                this.isLoading = false;
+                console.error(err);
+            },
+        });
+    }
 }
--- src/app/components/list-friends/list-friends.component.ts
+++ src/app/components/list-friends/list-friends.component.ts
@@ -1,25 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { FriendsService } from '../../services/friends/friends.service';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { FriendCardComponent } from '../friend-card/friend-card.component';
+import { MyFriendsResponse } from '../../models/response/myFriends.response';
+import { MetaModel } from '../../models/meta.model';
+import { PaginatorModule } from 'primeng/paginator';
+import { FriendRequestResponse } from '../../models/response/friendRequest.response';
 
 @Component({
     selector: 'app-list-friends',
     standalone: true,
-    imports: [NgForOf, FriendCardComponent],
+    imports: [NgForOf, FriendCardComponent, NgIf, PaginatorModule],
     templateUrl: './list-friends.component.html',
     styleUrl: './list-friends.component.css',
 })
-export class ListFriendsComponent {
+export class ListFriendsComponent implements OnInit {
     constructor(private friendsService: FriendsService) {}
 
     friends: User[] = [];
+    meta: MetaModel = new MetaModel();
+
+    currentPage: number = 1;
+
+    firstPage: number = 1;
+    totalPages: number = 1;
+    lastPage: number = 1;
+    firstPageUrl: string = '';
+    lastPageUrl: string = '';
+    nextPageUrl: string | null = '';
+    previousPageUrl: string | null = '';
+
+    ngOnInit() {
+        this.getFriends();
+    }
 
     getFriends() {
         this.friendsService.getFriends().subscribe({
-            next: (friends: any) => {
-                this.friends = friends;
+            next: (friends: MyFriendsResponse) => {
+                this.friends = friends.data;
+                this.meta = friends.meta;
+                this.currentPage = friends.meta.currentPage;
+
+                this.firstPage = friends.meta.firstPage;
+                this.totalPages = friends.meta.total;
+                this.lastPage = friends.meta.lastPage;
+                this.firstPageUrl = friends.meta.firstPageUrl;
+                this.lastPageUrl = friends.meta.lastPageUrl;
+                this.nextPageUrl = friends.meta.nextPageUrl;
+                this.previousPageUrl = friends.meta.previousPageUrl;
+                console.log(friends.data);
+            },
+            error: (error) => {
+                console.error(error);
+            },
+        });
+    }
+
+    onPageChange(event: any) {
+        if (event.page < 0 || event.page > this.totalPages) {
+            return;
+        }
+        if (event.page + 1 === this.currentPage) {
+            return;
+        }
+        this.friendsService.getFriends(event.page + 1, event.rows).subscribe({
+            next: (friends: MyFriendsResponse) => {
+                this.friends = friends.data;
+                this.meta = friends.meta;
+                this.currentPage = friends.meta.currentPage;
+                this.firstPage = friends.meta.firstPage;
+                this.totalPages = friends.meta.total;
+                this.lastPage = friends.meta.lastPage;
+                this.firstPageUrl = friends.meta.firstPageUrl;
+                this.lastPageUrl = friends.meta.lastPageUrl;
+                this.nextPageUrl = friends.meta.nextPageUrl;
+                this.previousPageUrl = friends.meta.previousPageUrl;
             },
             error: (error) => {
                 console.error(error);
