d7f37eb4c22c208a77fdd49a4c29cc2fcb0a9995
Emmanuel Boisset <72619789+ShadowZ11@users.noreply.github.com>
2024-06-01 19:30:21 +0200

    add delete account button (#9)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
+import { environment } from './environments/environment';
 
-inject();
+if (environment.production) {
+    inject();
+}
+
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
