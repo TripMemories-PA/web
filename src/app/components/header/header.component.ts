88938f1a9922d01cf21347b845363118b3a05516
Emmanuel Boisset <72619789+ShadowZ11@users.noreply.github.com>
2024-06-02 12:02:38 +0200

    Feature/refonte header (#10)
    * init new header refont
    
    * refonte header okay

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,11 +5,22 @@ import { NgIf, NgOptimizedImage } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MenubarModule, NgOptimizedImage, ButtonModule, NgIf],
+    imports: [
+        MenubarModule,
+        NgOptimizedImage,
+        ButtonModule,
+        NgIf,
+        InputIconModule,
+        InputTextModule,
+        ReactiveFormsModule,
+    ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
@@ -20,25 +31,31 @@ export class HeaderComponent implements OnInit {
     ) {}
 
     items: MenuItem[] | undefined;
+    showSearchInput: boolean = false;
 
     ngOnInit() {
         this.items = [
             {
-                label: 'Accueil',
-                icon: 'pi pi-fw pi-home',
+                label: 'Carte',
+                routerLink: ['/'],
+            },
+            {
+                label: 'Feed',
+                routerLink: ['/'],
+            },
+            {
+                label: 'Magasin',
                 routerLink: ['/'],
             },
             {
                 label: 'Profil',
-                icon: 'pi pi-fw pi-user',
                 routerLink: ['/profil'],
             },
-            {
-                label: 'Mes Amis',
-                icon: 'pi pi-fw pi-users',
-                routerLink: ['/friends'],
-            },
         ];
+    }
+
+    toggleSearchInput() {
+        this.showSearchInput = !this.showSearchInput;
     }
 
     login() {
