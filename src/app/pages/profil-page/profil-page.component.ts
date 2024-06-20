5640b0ff8b0f52ae071479e9c4464420cae1fc70
Emmanuel Boisset <72619789+ShadowZ11@users.noreply.github.com>
2024-06-20 23:47:42 +0200

    Feature/refonte profil (#21)
    * init refonte profil page
    
    * adding switch case
    
    * update UI

--- src/app/pages/profil-page/profil-page.component.ts
+++ src/app/pages/profil-page/profil-page.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ProfilService } from '../../services/profil/profil.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { ProfilInfoComponent } from '../../components/profil-info/profil-info.component';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { FriendsService } from '../../services/friends/friends.service';
 
 @Component({
     selector: 'app-profil-page',
     standalone: true,
-    imports: [ProfilInfoComponent],
+    imports: [ProfilInfoComponent, NgSwitch, NgSwitchCase],
     templateUrl: './profil-page.component.html',
     styleUrl: './profil-page.component.css',
 })
 export class ProfilPageComponent implements OnInit {
     profilPic: string | undefined = undefined;
+    nbrFriends: number = 0;
+
+    activeTab: string = 'posts';
+
+    setActiveTab(tab: string) {
+        this.activeTab = tab;
+    }
 
     constructor(
         private profilService: ProfilService,
+        private friendsService: FriendsService,
         private authServices: AuthService,
     ) {
         const user = JSON.parse(localStorage.getItem('user') as string);
@@ -36,6 +46,15 @@ export class ProfilPageComponent implements OnInit {
                 if (this.authServices.user?.avatar) {
                     this.profilPic = user.avatar.url;
                 }
+            },
+        });
+
+        this.friendsService.getFriends().subscribe({
+            next: (friends) => {
+                this.nbrFriends = friends.data.length;
+            },
+            error: (error) => {
+                console.error(error);
             },
         });
     }
