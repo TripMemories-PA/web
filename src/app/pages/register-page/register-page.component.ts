180758feabf41eab43125fd7c4b8f09532695f29
Emmanuel <manuboisset@gmail.com>
2024-06-24 20:32:05 +0200

    add redirection if already connected

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 import { NgOptimizedImage } from '@angular/common';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register-page',
@@ -10,4 +11,12 @@ import { RegisterFormComponent } from '../../components/register-form/register-f
     templateUrl: './register-page.component.html',
     styleUrl: './register-page.component.css',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent implements OnInit {
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        if (localStorage.getItem('user')) {
+            this.router.navigate(['/profil']);
+        }
+    }
+}
